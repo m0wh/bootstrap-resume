db9cec34679994da8353d25cfcb98051cad20c84
Malo de Widerspach <malo.dewiderspach@gmail.com>
2018-03-01 11:08:46 +0100

    added animations

--- script/scripts.js
+++ script/scripts.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
 
-  $('.in-bar').each(function(i) {
+
+  /* BARRES DE COMPETENCES */
+
+  $('.in-bar').each(function(i) { // Définit le temps d'animation pour chaque barre de compétence
     $(this).css({
       'transition-duration': 1000 + i*100 + "ms"
     });
-    console.log(500 + i*10);
   });
 
-  setTimeout(function () {
+  setTimeout(function () { // Anime les barres
     $('div[data-skill]').each(function(i) {
       $(this).children('.in-bar').css({
         'width': $(this).attr('data-skill')
@@ -15,4 +17,41 @@ $(document).ready(function() {
     });
   }, 200);
 
-});
+  /* ///////////////////// */
+
+
+
+  /* FADE-IN DES SECTIONS & EXPERIENCES */
+
+  $('section').each(function(i) {     // Définit le délai avant chaque animation et la durée
+    $(this).css({
+      'transition-delay': 100 + i*100 + "ms",
+      'transition-duration': 800 + i*200 + "ms"
+    });
+  });
+  $('.experience').each(function(i) {
+    $(this).css({
+      'transition-delay': 100 + i*200 + "ms",
+      'transition-duration': 800 + i*200 + "ms"
+    });
+
+  });
+
+  $('#sect-header').css('opacity', '1');
+  $('#sect-experience').css('opacity', '1');
+  $('#sect-competences').css('opacity', '1');
+  $('.experience').css('opacity', '1');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > $('#sect-languages').offset().top) {
+      $('#sect-languages').css('opacity', '1');
+    }
+    if ($(window).scrollTop() > $('#sect-hobbies').offset().top) {
+      $('#sect-hobbies').css('opacity', '1');
+    }
+  });
+
+  /* //////////////////// */
+
+
+})
